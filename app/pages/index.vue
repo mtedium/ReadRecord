9c692d8a260213a8c9c93206946fa2eef22ba08d
Mtedium <mtedium@outlook.com>
2026-05-10 23:39:18 +0800

    ✨ feat(ui): 主页新增表格+添加弹窗
    - 增加表格展示书籍列表
    - 新增弹窗表单支持添加书籍信息
    - 集成 Tailwind CSS 依赖

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,91 +1,87 @@
 <template>
   <div>
-    <h1>新增记录</h1>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="Date" width="180" />
+      <el-table-column prop="name" label="Name" width="180" />
+      <el-table-column prop="address" label="Address" />
+    </el-table>
 
-    <el-form :model="form" label-width="auto" style="max-width: 600px">
-      <el-form-item label="Activity name">
-        <el-input v-model="form.name" />
-      </el-form-item>
-      <el-form-item label="Activity zone">
-        <el-select v-model="form.region" placeholder="please select your zone">
-          <el-option label="Zone one" value="shanghai" />
-          <el-option label="Zone two" value="beijing" />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="Activity time">
-        <el-col :span="11">
+    <el-button type="primary" @click="dialogFormVisible = true" class="mt-5"
+      >添加书籍</el-button
+    >
+
+    <el-dialog v-model="dialogFormVisible" title="新增书籍" width="500">
+      <el-form :model="form" label-width="auto" style="max-width: 600px">
+        <el-form-item label="书名">
+          <el-input v-model="form.name" />
+        </el-form-item>
+
+        <el-form-item label="持续时间">
           <el-date-picker
-            v-model="form.date1"
-            type="date"
-            placeholder="Pick a date"
-            style="width: 100%"
+            v-model="form.duringtime"
+            type="daterange"
+            single-panel
+            range-separator="-"
+            start-placeholder="开始"
+            end-placeholder="结束"
           />
-        </el-col>
-        <el-col :span="2" class="text-center">
-          <span class="text-gray-500">-</span>
-        </el-col>
-        <el-col :span="11">
-          <el-time-picker
-            v-model="form.date2"
-            placeholder="Pick a time"
-            style="width: 100%"
-          />
-        </el-col>
-      </el-form-item>
-      <el-form-item label="Instant delivery">
-        <el-switch v-model="form.delivery" />
-      </el-form-item>
-      <el-form-item label="Activity type">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox value="Online activities" name="type">
-            Online activities
-          </el-checkbox>
-          <el-checkbox value="Promotion activities" name="type">
-            Promotion activities
-          </el-checkbox>
-          <el-checkbox value="Offline activities" name="type">
-            Offline activities
-          </el-checkbox>
-          <el-checkbox value="Simple brand exposure" name="type">
-            Simple brand exposure
-          </el-checkbox>
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="Resources">
-        <el-radio-group v-model="form.resource">
-          <el-radio value="Sponsor">Sponsor</el-radio>
-          <el-radio value="Venue">Venue</el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="Activity form">
-        <el-input v-model="form.desc" type="textarea" />
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">Create</el-button>
-        <el-button>Cancel</el-button>
-      </el-form-item>
-    </el-form>
+        </el-form-item>
+
+        <el-form-item label="状态">
+          <el-radio-group v-model="form.status">
+            <el-radio value="已读">已读</el-radio>
+            <el-radio value="未读">未读</el-radio>
+            <el-radio value="闲置">闲置</el-radio>
+          </el-radio-group>
+        </el-form-item>
+      </el-form>
+
+      <template #footer>
+        <div class="dialog-footer">
+          <!-- <el-button @click="dialogFormVisible = false">取消</el-button> -->
+          <el-button type="primary" @click="dialogFormVisible = false">
+            确定
+          </el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script lang="ts" setup>
 useHead({
-  title: '记录一切 - 主页',
-})
+  title: "记录一切 - 主页",
+});
+
+const dialogFormVisible = ref(false);
 
 // do not use same name with ref
 const form = reactive({
-  name: '',
-  region: '',
-  date1: '',
-  date2: '',
-  delivery: false,
-  type: [],
-  resource: '',
-  desc: '',
-})
+  name: "",
+  duringtime: "",
+  status: "",
+});
 
-const onSubmit = () => {
-  console.log('submit!')
-}
+const tableData = [
+  {
+    date: "2016-05-03",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
+  },
+  {
+    date: "2016-05-02",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
+  },
+  {
+    date: "2016-05-04",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
+  },
+  {
+    date: "2016-05-01",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
+  },
+];
 </script>
